bf6ee6b121336ca32cc498f23e4ad1d176110c0b
Ty Tran Dai <trandaity151@gmail.com>
2022-11-11 22:51:04 +0700

    Create header and source file for the clearNumberOnClock function

---
+++ Core/Inc/clearNumberOnClock.h
@@ -0,0 +1,15 @@
+/*
+ * clearNumberOnClock.h
+ *
+ *  Created on: Nov 11, 2022
+ *      Author: trand
+ */
+
+#ifndef INC_CLEARNUMBERONCLOCK_H_
+#define INC_CLEARNUMBERONCLOCK_H_
+
+#include "main.h"
+
+void clearNumberOnClock(int num);
+
+#endif /* INC_CLEARNUMBERONCLOCK_H_ */
---
+++ Core/Src/clearNumberOnClock.c
@@ -0,0 +1,12 @@
+/*
+ * clearNumberOnClock.c
+ *
+ *  Created on: Nov 11, 2022
+ *      Author: trand
+ */
+
+#include "clearNumberOnClock.h"
+
+void clearNumberOnClock(int num){
+
+}
